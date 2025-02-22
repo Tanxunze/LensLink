144d9dea1c7f23788a317d696ffe5a2eb295324a
Xunze Tan <24247472@studentmail.ul.ie>
2025-02-22 10:53:37 +0000

    fix some bugs

--- frontend/assets/js/loadComponents.js
+++ frontend/assets/js/loadComponents.js
@@ -11,14 +11,14 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 function updateAuthLinks() {
-  const authLinksElement = document.getElementById("authLinks");
-  const isLoggedIn = false; 
+    const authLinksElement = document.getElementById("authLinks");
+    const isLoggedIn = false; 
   const isPhotographer = false; //Todo
 
-  if (isLoggedIn) {
-    const dashboardUrl = isPhotographer
-      ? `${CONFIG.BASE_URL}pages/dashboard/photographer.html`
-      : `${CONFIG.BASE_URL}pages/dashboard/customer.html`;
+    if (isLoggedIn) {
+        const dashboardUrl = isPhotographer
+        ? `${CONFIG.BASE_URL}pages/dashboard/photographer.html`
+        : `${CONFIG.BASE_URL}pages/dashboard/customer.html`;
 
     authLinksElement.innerHTML = `
             <li class="nav-item">
@@ -28,14 +28,14 @@ function updateAuthLinks() {
                 <a class="nav-link" href="#" onclick="logout()">Logout</a>
             </li>
         `;
-  } else {
-    authLinksElement.innerHTML = `
-            <li class="nav-item">
-                <a class="nav-link" href="${CONFIG.BASE_URL}pages/auth/login.html">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="${CONFIG.BASE_URL}pages/auth/register.html">Register</a>
-            </li>
-        `;
-  }
+    } else {
+        authLinksElement.innerHTML = `
+                <li class="nav-item">
+                    <a class="btn btn-outline-success me-2" href="${CONFIG.BASE_URL}pages/auth/login.html">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-primary" href="${CONFIG.BASE_URL}pages/auth/register.html">Register</a>
+                </li>
+            `;
+    }
 }
